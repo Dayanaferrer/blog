977f5a75e6fb551ae100a0ef0a3c70cd3bfa12c0
Dayanaferrer <dayanaferreira2@gmail.com>
2024-04-16 16:52:14 -0300

    adicionando paginacao na pagina inicio

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from post.models import Post
 from inicio.models import Squad
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def inicio(request):
-    postagens = Post.objects.order_by('-dt_criacao')    
+    todas_postagens = Post.objects.order_by('-dt_criacao')
+    paginator = Paginator(todas_postagens, 2)  
+    numero_pagina = request.GET.get('pagina')
+    try:
+        postagens = paginator.page(numero_pagina)
+    except PageNotAnInteger:
+        postagens = paginator.page(1)
+    except EmptyPage:
+        postagens = paginator.page(paginator.num_pages)
     return render(request, 'galeria/inicio.html', {'postagens': postagens})
 
 def sobre(request):    
